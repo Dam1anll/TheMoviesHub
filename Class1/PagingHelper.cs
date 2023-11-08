3894f91fbae7d8186797c20ee0ac7030486c5b52
Damian <damiianll20@gmail.com>
2023-11-08 13:20:45 -0700

    Agregar archivos de proyecto.

---
+++ Class1/PagingHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheMoviesHub.Class1
+{
+    public static class PagingHelper
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl) 
+        {
+            StringBuilder result = new StringBuilder();
+            string anchorInnerHtml = "";
+            for(int i = 1; i <= pagingInfo.TotalPages; i++) 
+            {
+                TagBuilder tag = new TagBuilder("a");
+                anchorInnerHtml = AnchorInnerHtml(i, pagingInfo);
+
+                if (anchorInnerHtml == "..")
+                    tag.MergeAttribute("href", "#");
+                else
+                    tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = anchorInnerHtml;
+                if(i == pagingInfo.CurrentPage) 
+                {
+                    tag.AddCssClass("active");
+                }
+                tag.AddCssClass("pagina");
+                if(anchorInnerHtml == "")
+                    result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+
+        public static string AnchorInnerHtml(int i, PagingInfo pagingInfo) 
+        {
+            string anchorInnerHtml = "";
+            if (pagingInfo.TotalPages <= 10)
+                anchorInnerHtml = i.ToString();
+            else 
+            {
+                if(pagingInfo.CurrentPage <= 5) 
+                {
+                    if ((i <= 8) || (i == pagingInfo.TotalPages))
+                        anchorInnerHtml = i.ToString();
+                    else if(i == pagingInfo.TotalPages -1)
+                        anchorInnerHtml = "..";
+                }
+            }
+            return anchorInnerHtml;
+        }
+        
+    }
+}
---
+++ Controllers/TmdbApiControllerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using TheMoviesHub.Models;
+
+namespace TheMoviesHub.Controllers
+{
+    public class TmdbApiControllerController : Controller
+    {
+       public ActionResult Index(string peopleName, int? page) 
+       {
+            if(page !=  null) 
+                CallAPI(peopleName, Convert.ToInt32(page));
+
+            Models.TheMovieDb theMovieDb = new Models.TheMovieDb();
+            theMovieDb.searchText = peopleName;
+            return View(theMovieDb);
+       }
+    }
+
+    [HttpPost]
+
+    public ActionResult Index(Models.TheMovieDb theMovieDb, string searchText) 
+    {
+        if (ModelState.IsValid) 
+        {
+            CallAPI(searchText, 0);
+        }
+        return View(theMovieDb);
+    }
+
+    public void CallAPI(string searchText, int page) 
+    {
+        int pageNo = Convert.ToInt32(page) == 0 ? 1 : Convert.ToInt32(page);
+
+        /*Calling API https://developers.themoviedb.org/3/search/search-poeple */
+    }
+}
